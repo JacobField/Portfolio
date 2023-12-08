0a2b352a8898ccd475eef06a30becb3e5f0770f9
jacobfield <jacobprogrammes@gmail.com>
2023-12-08 17:33:28 +0000

    bringing up to date with main

---
+++ src/comps/TeckStack.js
@@ -0,0 +1,28 @@
+import { IoLogoJavascript } from "react-icons/io";
+import { TbBrandTypescript } from "react-icons/tb";
+import { FaReact } from "react-icons/fa";
+import { BsFiletypeSql } from "react-icons/bs";
+import { SiExpress } from "react-icons/si";
+import { IoLogoNodejs } from "react-icons/io";
+import { FaGithub } from "react-icons/fa";
+import { SiPlaywright } from "react-icons/si";
+import { SiVitest } from "react-icons/si";
+import { FaHtml5 } from "react-icons/fa";
+import { TbFileTypeCss } from "react-icons/tb";
+export function TechStack() {
+  return (
+    <div>
+      <IoLogoJavascript className="icon" />
+      <TbBrandTypescript className="icon" />
+      <FaReact className="icon" />
+      <FaHtml5 className="icon" />
+      <TbFileTypeCss className="icon" />
+      <BsFiletypeSql className="icon" />
+      <SiExpress className="icon" />
+      <IoLogoNodejs className="icon" />
+      <FaGithub className="icon" />
+      <SiPlaywright />
+      <SiVitest />
+    </div>
+  );
+}
